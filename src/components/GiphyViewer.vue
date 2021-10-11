0a88271b34b0de45bb0926f691794db927b5bc6d
adamxzz <71635657+adamxzz@users.noreply.github.com>
2021-10-11 14:54:09 +0100

    added search and random gifs

--- src/components/GiphyViewer.vue
+++ src/components/GiphyViewer.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <div class="search-box">
+      <input type="text" v-model="term" v-on:keyup.enter="searchGiphy()" />
+      <b-button class="float-end" variant="primary" @click="searchGiphy()"
+        >Search</b-button
+      >
+    </div>
+    <div>
+      <!--Activity 1: Trending gifs button -->
+      <button class="float-end" variant="secondary" @click="trendingGiphy()">
+        Trending
+      </button>
+    </div>
+          <!-- Activity 2: Random gifs button -->
+      <button class="float-end" variant="secondary" @click="randomGiphy()">
+        Random Gif
+      </button>
+
     <b-card-group columns>
       <b-card
         v-for="gif in gifs"
@@ -29,16 +46,79 @@ export default {
     };
   },
   mounted() {
-    axios
-      .get(`${GIPHY_URL}/trending?api_key=${API_KEY}`)
-      .then((response) => {
-        console.log(response.data.data);
-        this.gifs = response.data.data;
-      })
-      .catch((error) => console.log(error));
+this.trendingGiphy();
   },
-  methods: {},
+  methods: {
+    searchGiphy() {
+      if (!this.term) {
+        alert("Please enter a search term");
+        this.$byToast.toast("Please enter a search term", {
+          title: "Warning",
+          variant: "danger",
+          toaster: "b-toaster-top-center",
+          autoHideDelay: 5000,
+          solid: true,
+        });
+        return;
+      }
+      axios
+        .get(`${GIPHY_URL}/search?api_key=${API_KEY}&q=${this.term}&limit=20`)
+        .then((response) => {
+          console.log(response.data.data);
+          this.gifs = response.data.data;
+        })
+        .catch((error) => console.log(error));
+    },
+    trendingGiphy() {
+      axios
+        .get(`${GIPHY_URL}/trending?api_key=${API_KEY}`)
+        .then((response) => {
+          console.log(response.data.data);
+          this.gifs = response.data.data;
+        })
+        .catch((error) => console.log(error));
+    },
+    randomGiphy() {
+        axios
+        .get(`${GIPHY_URL}/random?api_key=${API_KEY}`)
+        .then((response) => {
+          console.log(response.data.data);
+this.gifs = [response.data.data ]        })
+        .catch((error) => console.log(error));
+    },
+  },
 };
 </script>
 <style>
+.card {
+  margin-bottom: 20px;
+}
+@media (min-width: 34em) {
+  .card-columns {
+    -webkit-column-count: 2;
+    -moz-column-count: 2;
+    column-count: 2;
+  }
+}
+@media (min-width: 48em) {
+  .card-columns {
+    -webkit-column-count: 3;
+    -moz-column-count: 3;
+    column-count: 3;
+  }
+}
+@media (min-width: 62em) {
+  .card-columns {
+    -webkit-column-count: 4;
+    -moz-column-count: 4;
+    column-count: 4;
+  }
+}
+@media (min-width: 75em) {
+  .card-columns {
+    -webkit-column-count: 5;
+    -moz-column-count: 5;
+    column-count: 5;
+  }
+}
 </style>
